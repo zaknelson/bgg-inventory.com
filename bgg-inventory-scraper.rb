a2b19534c3c7170e354bfa515b8f4f2b9b8ecd0c
Zak Nelson <zak.nelson@gmail.com>
2013-09-21 18:02:15 -0700

    Push endpoint which will refresh the data.

--- bgg-inventory-scraper.rb
+++ bgg-inventory-scraper.rb
@@ -115,7 +115,7 @@ end
 
 def main
 	games = fetch_top_games(500)
-	File.open("../model/games.json", "w") { |file| file.write(games.to_json) }
+	File.open("model/games.json", "w") { |file| file.write(games.to_json) }
 end
 
 main()
--- bgg-inventory.go
+++ bgg-inventory.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"compress/gzip"
+	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
+	"time"
 )
 
 type Page struct {
@@ -57,8 +61,35 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	t.ExecuteTemplate(w, "index.html", page)
 }
 
+func UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	startedInfo, lastStartedErr := os.Stat("model/started.txt")
+	startedTimeAgo := time.Now().Sub(startedInfo.ModTime())
+
+	finishedInfo, finishedErr := os.Stat("model/games.json")
+	finishedTimeAgo := time.Now().Sub(finishedInfo.ModTime())
+
+	if  lastStartedErr == nil && finishedErr == nil && 
+		startedTimeAgo.Minutes() > 20 && finishedTimeAgo.Hours() > 24 {
+		go func() {
+			ioutil.WriteFile("model/started.txt", nil, 0644)
+			command := exec.Command("ruby", "bgg-inventory-scraper.rb")
+			err := command.Run()
+			if err != nil {
+				fmt.Printf("Error occurred with first fetch attempt, try again")
+				time.Sleep(5 * time.Second)
+				command := exec.Command("ruby", "bgg-inventory-scraper.rb")
+				command.Run()
+				// If we fail again, oh well will just have to try again later
+			}
+		}()
+	}
+
+	fmt.Fprintf(w, "%s", finishedInfo.ModTime().Format(time.ANSIC))
+}
+
 func init() {
 	http.HandleFunc("/", makeGzipHandler(IndexHandler))
+	http.HandleFunc("/update", UpdateHandler)
 	serveSingle("/api/v1/games", "model/games.json")
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css/"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./js/"))))
